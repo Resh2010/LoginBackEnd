1537c4749ee8526bd50b9dfea5c0a1d0d47423a4
Resh2010 <reshma.ramesh@hrblock.com>
2022-08-23 13:24:57 +0530

    LoginController

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoginBackend.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public bool LoginCheck(string username,string password)
+        {
+            if(username=="reshma" && password=="resh123")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
